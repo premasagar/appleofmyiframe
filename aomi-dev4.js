69e3b8deadd9434d5dff74bed5e2bf70d1c04a96
Premasagar Rose <premasagar@prem.(none)>
2009-12-14 06:08:44 -0500

    additional methods added

--- aomi-dev4.js
+++ aomi-dev4.js
@@ -34,31 +34,12 @@
 
     //    Id of hidden div
     var
-        hiddenDivId = 'appleofmyiframe',
+        win = window,
+        document = win.document,
+        
         AppleOfMyIframe = new JqueryClass({
-            onload : function(callback){
-                var
-                    that = this,
-                    iframe = this[0],
-                    onload = function(){
-                        callback.call(that);
-                    };
-                // W3C
-                iframe.onload = onload;
-                
-                // IE (+ Opera?)
-                if (iframe.attachEvent){
-                    iframe.attachEvent('onload', onload);
-                }
-                return this;
-            },
-            
-            ready : function(callback){
-                $.event.add(this, 'iframe.ready', callback); // We use $.event.add instead of this.bind()
-                return this;
-            },
             initialize : function(args){
-                var that = this, headContents, bodyContents, optionsFound, callback, attr;
+                var aomi = this, headContents, bodyContents, optionsFound, callback, attr;
                 
                 this.options = {
                     attr:{
@@ -77,7 +58,7 @@
                     }
                     else if (!optionsFound && typeof arg === 'object' && !isJQuery(arg) && !isElement(arg)){
                         optionsFound = true;
-                        $.extend(true, that.options, arg);
+                        $.extend(true, aomi.options, arg);
                     }
                     else if (!bodyContents && typeof arg !== 'undefined'){
                         bodyContents = arg;
@@ -94,15 +75,11 @@
                     attr.src = bodyContents;
                 }
                 
-                this.onload(function(){
-                    $.event.trigger('iframe.ready', null, this); // Use $.event.trigger() instead of this.trigger()
-                });
-                
                 if ((bodyContents && !isUrl(bodyContents)) || headContents){                
                     this.ready(function(){
                         this
-                            .prepareDocument()
-                            .setContents();
+                            ._prepareDocument()
+                            .setContents(headContents, bodyContents);
                     });
                 }
                 if (callback){
@@ -112,53 +89,20 @@
                 // Absorb the iframe
                 $.fn.init.call(this, '<iframe></iframe>').attr(attr);
                 
-                return this;
-            },
-            
-            prepareDocument : function(){
-                var doc = this.document();            
-                doc.open();
-                doc.close();
-                this.body().css({margin:0, padding:0});
-                return this;
-            },
-            
-            setContents : function(headContents, bodyContents){
-                if (!bodyContents){
-                    bodyContents = headContents;
-                    headContents = false;
-                }
+                // Setup 'ready' event, for when iframe element fires 'load' event
+                this._onload(function(){
+                    $.event.trigger('iframe.ready', null, this); // Use $.event.trigger() instead of this.trigger()
+                });
                 
-                // Set body contents, and cache
-                if (bodyContents){
-                    this.body(bodyContents);
-                    this.data('body', this.document().body());
-                }
-                // Set head contents, and cache
-                if (headContents){
-                    this.head(headContents);
-                    this.data('head', this.document().head());
-                }
-                if (this.options.autoresize){
-                    this.matchSize();
-                }
                 return this;
             },
         
-            appendTo : function(obj){
-                var
-                    ie6 = ($.browser.msie && parseInt($.browser.version, 10) === 6),
-                    className;
+            $ : function(arg) {
+                return $(arg, this.document());
+            },
             
-                $.fn.appendTo.call(this, obj);
-                if (!this.attr('src') || this.attr('src') === 'about:blank') {
-                    this._swapDocuments();
-                }
-                // IE6 repaint hack for external src iframes
-                else if (ie6) {
-                    className = 'ie6hack';
-                    this.addClass(className).removeClass(className);
-                }
+            ready : function(callback){
+                $.event.add(this, 'iframe.ready', callback); // We use $.event.add instead of this.bind()
                 return this;
             },
         
@@ -202,89 +146,118 @@
                 }
                 return this;
             },
-        
-            $ : function(arg) {
-                return $(arg, this.document());
-            },
-        
-            //    Get hidden div. Insert if necessary.
-            _hiddenDiv : function()
-            {
-                var div = $('#'+ hiddenDivId);
-        
-                if (div.length){
-                    return div;
-                }
             
-                return $('<div></div>')
-                        .attr('id', hiddenDivId)
-                        .css({
-                            overflow:'hidden',
-                            width:'1px',
-                            height:'1px',
-                            //visibility:'hidden',
-                            position:'absolute',
-                            left:'-1000px',
-                            top:'-1000px'
-                        })
-                        .appendTo('body');
+            setContents : function(headContents, bodyContents){
+                if (!bodyContents){
+                    bodyContents = headContents;
+                    headContents = false;
+                }
+                
+                // Set body contents, and cache
+                if (bodyContents){
+                    this.body(bodyContents);
+                    this.data('body', this.body());
+                }
+                // Set head contents, and cache
+                if (headContents){
+                    this.head(headContents);
+                    this.data('head', this.head());
+                }
+                if (this.options.autoresize){
+                    this.matchSize();
+                }
+                return this;
             },
         
-            //     Remove hidden div if empty
-            _checkHiddenDiv : function()
-            {
-                // TODO: 
+            appendTo : function(obj){
+                var
+                    ie6 = ($.browser.msie && win.parseInt($.browser.version, 10) === 6),
+                    className;
+            
+                $.fn.appendTo.call(this, obj);
+                if (!this.attr('src') || this.attr('src') === 'about:blank') {
+                    this._swapDocuments();
+                }
+                // IE6 repaint hack for external src iframes
+                else if (ie6) {
+                    className = 'ie6hack';
+                    this.addClass(className).removeClass(className);
+                }
+                return this;
             },
+            
+            _prepareDocument : function(){
+                var doc = this.document();            
+                doc.open();
+                doc.close();                
+                this.body()
+                    .css({margin:0, padding:0});
+                return this;
+            },
+            
+            _onload : function(callback){
+                var
+                    aomi = this,
+                    iframe = this[0],
+                    onload = function(){
+                        callback.call(aomi);
+                    };
+                // W3C
+                iframe.onload = onload;
+                
+                // IE (+ Opera?)
+                if (iframe.attachEvent){
+                    iframe.attachEvent('onload', onload);
+                }
+                return this;
+            },
+            
+            _moveNode : (function(){
+                var method;
+                
+                if (document.adoptNode){
+                    method = 'adoptNode';
+                }
+                else if (document.importNode){
+                    method = 'importNode';
+                }
+                else if (document.cloneNode){
+                    method = 'cloneNode';
+                }
+                else {
+                    method = false;
+                }
+                
+                return function(document, node){
+                    return document && node && method ?
+                        document[method](node, true):
+                        false;
+                };
+            }()),
         
             //    Insert body and head elements from old document into new document
-            _swapDocuments : function()
-            {
-                var newDocument, oldDocument, old_body, new_body, old_head, new_head, moveNode;
+            _swapDocuments : function(){
+                var
+                    doc = this.document(),
+                    old_body,
+                    old_head;
         
                 if ($.browser.msie){
                     return true;
                 }
-                newDocument = this.document();
-                old_body = this.data('body');
-            
-                moveNode = this._moveNode;            
-                if (typeof moveNode === 'undefined'){
-                    if (newDocument.adoptNode){
-                        moveNode = 'adoptNode';
-                    }
-                    else if (newDocument.importNode){
-                        moveNode = 'importNode';
-                    }
-                    else if (newDocument.cloneNode){
-                        moveNode = 'cloneNode';
-                    }
-                    else {
-                        moveNode = false;
-                    }
-                    this._moveNode = moveNode;
+                old_body = this._moveNode(doc, this.data('body'));
+                old_head = this._moveNode(doc, this.data('head'));
+                
+                if (old_body){
+                    this.body().replaceWith(old_body);
+                    this.data('body', old_body.clone(true)); // TODO: Does this need to be a clone?
                 }
-                if (!moveNode){
-                    return false;
+                if (old_head){
+                    this.head().replaceWith(old_head);
+                    this.data('head', old_head.clone(true)); // TODO: Does this need to be a clone?
                 }
-                
-                if (old_body){                
-                    //console.dir(old_body.innerHTML);
-                    oldDocument = old_body.ownerDocument;
-                
-                    newDocument.open();
-                    newDocument.close();
-                
-                    //var old_body = oldDocument.body;
-                    new_body = newDocument.body;
-                    newDocument[moveNode](old_body, true); // 'true' has no effect for adoptNode, but is used for importNode and cloneNode
-                    new_body.parentNode.replaceChild(old_body, new_body);
-                
-                    old_head = oldDocument.getElementsByTagName('head')[0];
-                    new_head = newDocument.getElementsByTagName('head')[0];
-                    newDocument[moveNode](old_head, true);
-                    new_head.parentNode.replaceChild(old_head, new_head);
-                }
-                this.data('body', newDocument.body.cloneNode(true));
+
+                return this;
             }
         });
     
@@ -296,3 +269,5 @@
     });
     
 }(jQuery));
+
+/*jslint onevar: true, browser: true, devel: true, undef: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true */
