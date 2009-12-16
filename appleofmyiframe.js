5de2736e40eb45f54e2ec07093b6004977b85a69
Premasagar Rose <premasagar@prem.(none)>
2009-12-16 14:17:07 -0500

    Improved logic in _maintainDocumentContents() (formerly called _pinIframeContents)

--- appleofmyiframe.js
+++ appleofmyiframe.js
@@ -20,7 +20,7 @@
         return $.extend(
             function(){
                 if (this.initialize){
-                    this.initialize.apply(this, arguments);
+                    this.initialize.apply(this, $.makeArray(arguments));
                 }
             },
             {
@@ -33,7 +33,6 @@
 
     var
         win = window,
-        document = win.document,
         msie = $.browser.msie,
         ie6 = (msie && win.parseInt($.browser.version, 10) === 6),
         
@@ -42,6 +41,7 @@
                 var aomi = this, headContents, bodyContents, optionsFound, callback, attr;
                 
                 this.options = {
+                    // TODO: Verify which of these can be moved to CSS style property (allowtransparency, although non-standard, is required for IE to support iframes with transparent bodies
                     attr:{
                         scrolling:'no',
                         frameborder:0,
@@ -81,10 +81,12 @@
                             if (!msie){
                                 this._prepareDocument();
                             }
-                            this.setContents(headContents, bodyContents);
+                            this
+                                ._trimBody()
+                                .contents(headContents, bodyContents);
                         })
                         .load(function(){
-                            this._pinIframeContent();
+                            this._maintainDocumentContents();
                         });
                 }
                 if (callback){
@@ -107,16 +109,15 @@
                 return $(arg, this.document());
             },
             
-            // TODO: make this fire only on the first onload event
             ready : function(callback){
                 var aomi = this;
                 return this.load(function outerCallback(){
                     $.event.remove(aomi, 'iframe.ready', outerCallback);
-                    callback.apply(aomi, arguments);
+                    callback.apply(aomi, $.makeArray(arguments));
                 });
             },
             
-            // TODO: make this fire on every onload event (as the current .ready() method already does)
+            // TODO: use a different custom event type to the .ready() method (e.g. iframe.load - but test if this triggers the load() method)
             load: function(callback){
                 $.event.add(this, 'iframe.ready', callback); // We use $.event.add instead of this.bind()
                 return this;
@@ -167,7 +168,7 @@
                 return this;
             },
             
-            setContents : function(headContents, bodyContents){
+            contents : function(headContents, bodyContents){
                 if (typeof bodyContents === 'undefined'){
                     bodyContents = headContents;
                     headContents = false;
@@ -189,10 +190,7 @@
             },
         
             appendTo : function(obj){
-                var
-                    src = this.attr('src'),
-                    className;
-            
+                var className;            
                 $.fn.appendTo.call(this, obj);
                 
                 // IE6 repaint hack for external src iframes
@@ -223,7 +221,7 @@
             },
             
             // Hack to prevent situation where an iframe with an external src is on page, as well as an injected iframe; if the iframes are moved in the DOM and the page reloaded, then the contents of the external src iframe may be duplicated into the injected iframe (seen in FF3.5 and others). This function re-appplies the 'about:blank' src attribute of injected iframes, to force a reload of its content
-            _enforceSrc : function(){
+            _enforceBlankDoc : function(){
                 if (this.hasBlankSrc() && this.hasExternalDocument()){
                     this.attr('src', 'about:blank');
                     return true; // iframe is being reloaded
@@ -231,56 +229,85 @@
                 return false; // iframe doesn't require intervention
             },
             
-            _pinIframeContent : function(){            
+            // If an injected iframe fires the onload event move than once, then its content will be lost, so we need to pull the nodes from  IE doesn't fire onload event more than once.
+            _maintainDocumentContents : function(){
 	            var
+	                fn = this._maintainDocumentContents,
 	                doc = this.document(),
 	                htmlElement = $(doc).find('html'),
 	                oldHtmlElement = this._oldHtmlElement,
 	                oldHead,
-	                oldBody;
+	                oldBody,
+	                method,
+	                appendWith;
 
 	            if (oldHtmlElement){
 		            oldHead = oldHtmlElement.find('head');
 		            oldBody = oldHtmlElement.find('body');
 		            htmlElement.empty();
-
-                    // Priority 1: adoptNode
-                    if (doc.adoptNode){
-                        htmlElement
+		            method = fn.appendMethod;
+		            
+		            // Re-usable append function, for trying different DOM methods            
+		            appendWith = function(method){
+                        function appendNode(node){
+                            htmlElement.append(
+                                method ?
+                                    doc[method](node, true) :
+                                    node
+                            );
+                        }
+                        // NOTE: this.reload() not yet implemented
+                        if (method === 'reload'){
+                            this.reload();
+                        }
+                        else {                        
                             // NOTE: even if oldHead or oldBody are null, or the adoptNode fails, this won't error
-                            .append(doc.adoptNode(oldHead[0]))
-                            .append(doc.adoptNode(oldBody[0]));
+                            appendNode(oldHead[0]);
+                            appendNode(oldBody[0]);
+                        }
+                        return method;
+                    };
+                    
+                    // If we've already determined the method to use, then use it
+                    if (method){
+                        appendWith(method);
                     }
+                    // If not, then cycle through the different options
                     else {
-                        // Priority 2: appendChild 
-                        // attempt to append nodes from the other document; technically against the DOM spec, but supported by FF2 et al
-                        try {
-                            htmlElement
-                                .append(oldHead)
-                                .append(oldBody);
+                        // #1: adoptNode
+                        if (doc.adoptNode){
+                            method = appendWith('adoptNode');
                         }
-                        catch(e){
-                            // Priority 3: importNode
-                            // this, and the remaining steps, will clone the nodes, so any references to nodes will be broken
-                            if (doc.importNode){
-                                htmlElement
-                                    .append(doc.importNode(oldHead[0]), true)
-                                    .append(doc.importNode(oldBody[0]), true);
+                        else {
+                            // #2: appendChild 
+                            // append nodes straight from the other document; technically against the DOM spec, but supported by FF2 et al
+                            try {
+                                method = appendWith(null);
                             }
-                            else {
-                                // Priority 4: cloneNode
-                                // if 2) appendChild didn't work, then this probably won't either
-                                try {
-                                    htmlElement
-                                        .append(doc.cloneNode(oldBody[0]), true) // TODO: any difference with oldBody.clone(true) ?
-                                        .append(doc.cloneNode(oldBody[0]), true);
+                            catch(e){
+                                // #3: importNode
+                                // this and remaining steps will clone the nodes, so any references to nodes will be broken
+                                if (doc.importNode){
+                                    method = appendWith('importNode');
                                 }
-                                catch(e){
-                                    // Priority 5: reload iframe
-                                    this.reload();
+                                else {
+                                    // #4: cloneNode
+                                    // if 2) appendChild didn't work, then this probably won't either
+                                    try {
+                                        method = appendWith('cloneNode');
+                                    }
+                                    catch(e2){
+                                        // #5: reload iframe
+                                        // TODO: Add reload() method to re-initialize $.iframe() with the original constructor arguments
+                                        /*                                        
+                                        method = appendWith('reload');
+                                        */
+                                    }
                                 }
                             }
                         }
+                        // The append method will be stored as a property of the _maintainDocumentContents method in the AppleOfMyIframe prototype, so it only needs to run once on the first iframe, to determine the best method to use.
+                        fn.appendMethod = method;
                     }
 	            }
 	            this._oldHtmlElement = htmlElement;
@@ -292,10 +319,13 @@
                 doc.open();
                 doc.write('<head></head><body></body>');
                 doc.close();
-				
+                                
+                return this;
+            },
+            
+            _trimBody : function(){
                 this.body()
                     .css({margin:0, padding:0});
-                
                 return this;
             },
             
@@ -303,13 +333,13 @@
                 var
                     aomi = this,
                     iframe = this[0],
-                    enforceSrcDone = false,
+                    enforceBlankDocDone = false,
                     
                     onload = function(){
                         var iframeNeedsReload = false;
-                        if (!enforceSrcDone){
-                            iframeNeedsReload = aomi._enforceSrc();
-                            enforceSrcDone = true;
+                        if (!enforceBlankDocDone){
+                            iframeNeedsReload = aomi._enforceBlankDoc();
+                            enforceBlankDocDone = true;
                         }
                         if (!iframeNeedsReload){
                             callback.call(aomi);
@@ -324,57 +354,6 @@
                 }
                 return this;
             }
-            
-            /*
-            _moveNode : (function(){
-                var method;
-                
-                if (document.adoptNode){
-                    method = 'adoptNode';
-                }
-                else if (document.importNode){
-                    method = 'importNode';
-                }
-                else if (document.cloneNode){
-                    method = 'cloneNode';
-                }
-                else {
-                    method = false;
-                }
-                
-                return function(document, $node){
-                    return document && $node && $node[0] && method ?                    
-                        document[method]($node[0], true):
-                        false;
-                };
-            }()),
-        
-            //    Insert body and head elements from old document into new document
-            _swapDocuments : function(){
-                var
-                    newDocument = this.document(),
-                    oldBody,
-                    oldHead;
-        
-                if ($.browser.msie){
-                    return true;
-                }
-                
-                oldBody = this._moveNode(newDocument, this.data('body'));
-                oldHead = this._moveNode(newDocument, this.data('head'));
-                
-                if (oldBody){
-                    this.body().replaceWith(oldBody);
-                    this.data('body', $(oldBody).clone(true)); // TODO: Does this need to be a clone?
-                }
-                if (oldHead){
-                    this.head().replaceWith(oldHead);
-                    this.data('head', $(oldHead).clone(true)); // TODO: Does this need to be a clone?
-                }
-
-                return this;
-            }
-            */
         });
     
     // Extend jquery with the iframe method
